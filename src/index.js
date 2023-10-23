539b3372ae987f7a9d5f20b1c302edb494ac8d7b
LpxsBr <anselmolopes.an@gmail.com>
2023-10-23 20:04:31 -0300

    cleaning index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from 'cors'
-import { compareSync, hashSync } from 'bcrypt'
-import { generateAuth, verifyAuth } from "./config/jwt.js";
-import { User } from "./app/database/Models/User.js";
+import { verifyAuth } from "./config/jwt.js";
 import AuthController from "./app/controllers/AuthController.js";
 import UserController from "./app/controllers/UserController.js";
 
@@ -13,19 +11,11 @@ const port = process.env.SERVER_PORT || 8080
 router.use(express.json());
 router.use(cors())
 
-router.get('/', (req, res, next) => {
-
-})
-
+router.get('/', (req, res, next) => res.status(200).json({ hello: 'world!' }))
 router.post('/user', UserController.create)
 router.post('/auth', AuthController.Auth)
 router.use(verifyAuth) // my auth middleware
 router.get('/user/:id', UserController.show)
 router.put('/user/:id', UserController.update)
-
-router.get('/callback', async (req, res, next) => {
-
-})
-
 
 router.listen(port, (req, res, next) => console.log(`RUNNING ON HTTP://LOCALHOST:${port}`));
